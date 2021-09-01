a65bdd53812224fcc60c23e4933eb7f316dac938
Eric Soppi <eric.soppi@gmail.com>
2021-09-01 08:46:12 -0600

    Implement list notation printer

--- internal/core/constants/reserved_value_constants.go
+++ internal/core/constants/reserved_value_constants.go
@@ -2,3 +2,7 @@ package constants
 
 const T = "T"
 const NIL = "NIL"
+
+const NewLine = "\n"
+const Space = " "
+const ListDelimiter = " . "
--- internal/printer/list_notation_printer.go
+++ internal/printer/list_notation_printer.go
@@ -2,7 +2,9 @@ package printer
 
 import (
 	"lisp_lambda-go/internal"
+	"lisp_lambda-go/internal/core/constants"
 	"lisp_lambda-go/internal/core/datamodels"
+	"strings"
 )
 
 type listNotationPrinter struct {}
@@ -12,11 +14,37 @@ func NewListNotationPrinter() *listNotationPrinter {
 }
 
 func (l listNotationPrinter) PrintAllInListNotation(nodes []datamodels.Node) string {
-	panic("implement me")
+	var strs []string
+	for _, node := range nodes {
+		str := l.PrintInListNotation(node)
+		strs = append(strs, str)
+	}
+	return strings.Join(strs, constants.NewLine)
 }
 
 func (l listNotationPrinter) PrintInListNotation(node datamodels.Node) string {
-	panic("implement me")
+	expressionNode, isExpressionNode := node.(*datamodels.ExpressionListNode)
+	if isExpressionNode {
+		result := constants.OPEN_TOKEN_STR
+		for i := 0; i < len(expressionNode.GetChildren()) - 1; i++ {
+			result += l.PrintInListNotation(expressionNode.GetChildren()[i])
+			result += constants.Space
+		}
+		result = strings.TrimSpace(result)
+		lastChild := expressionNode.GetChildren()[len(expressionNode.GetChildren()) - 1]
+		atomNode, isAtomNode := lastChild.(*datamodels.AtomNode)
+		if isAtomNode && atomNode.GetValue() == constants.NIL {
+			result += constants.CLOSE_TOKEN_STR
+		} else {
+			result += constants.ListDelimiter
+			result += l.PrintInListNotation(lastChild)
+			result += constants.CLOSE_TOKEN_STR
+		}
+		return result
+	} else {
+		atomNode, _ := node.(*datamodels.AtomNode)
+		return atomNode.GetValue()
+	}
 }
 
 var _ internal.ListNotationPrinter = &listNotationPrinter{}
